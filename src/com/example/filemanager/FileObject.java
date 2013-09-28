aead637b7d929f139014facb1a816fd78d84ad28
jitender chaudhary <29jitender@gmail.com>
2013-09-28 23:34:01 +0530

    removed unused variables

--- src/com/example/filemanager/FileObject.java
+++ src/com/example/filemanager/FileObject.java
@@ -67,7 +67,7 @@ public class FileObject
         int unit = si ? 1000 : 1024;// 1024 bit to byte or for
         if (bytes < unit) return bytes + " B";
         int exp = (int) (Math.log(bytes) / Math.log(unit));
-        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + ("");
         return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
     }
 
